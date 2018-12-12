9d84a25468dee7de0f12cec50861d51f685a7d7b
yangc <nevase@foxmail.com>
2018-12-12 20:51:01 +0800

    升级身份证校验编号配置

--- src/main/java/com/imadcn/framework/common/identity/IDCardUtil.java
+++ src/main/java/com/imadcn/framework/common/identity/IDCardUtil.java
@@ -9,41 +9,33 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class IDCardUtil {
-	/*********************************** 身份证验证开始 ****************************************/	
+	/***********************************
+	 * 身份证验证开始
+	 ****************************************/
 	/**
-	 * 身份证号码验证 
-	 * 1、号码的结构
-	 * 公民身份号码是特征组合码，由十七位数字本体码和一位校验码组成。排列顺序从左至右依次为：六位数字地址码，
-	 * 八位数字出生日期码，三位数字顺序码和一位数字校验码。
-	 * 2、地址码(前六位数） 
-	 * 表示编码对象常住户口所在县(市、旗、区)的行政区划代码，按GB/T2260的规定执行。 
-	 * 3、出生日期码（第七位至十四位）
-	 * 表示编码对象出生的年、月、日，按GB/T7408的规定执行，年、月、日代码之间不用分隔符。 
-	 * 4、顺序码（第十五位至十七位）
-	 * 表示在同一地址码所标识的区域范围内，对同年、同月、同日出生的人编定的顺序号，
-	 * 顺序码的奇数分配给男性，偶数分配给女性。 
-	 * 5、校验码（第十八位数）
+	 * 身份证号码验证 1、号码的结构 公民身份号码是特征组合码，由十七位数字本体码和一位校验码组成。排列顺序从左至右依次为：六位数字地址码，
+	 * 八位数字出生日期码，三位数字顺序码和一位数字校验码。 2、地址码(前六位数）
+	 * 表示编码对象常住户口所在县(市、旗、区)的行政区划代码，按GB/T2260的规定执行。 3、出生日期码（第七位至十四位）
+	 * 表示编码对象出生的年、月、日，按GB/T7408的规定执行，年、月、日代码之间不用分隔符。 4、顺序码（第十五位至十七位）
+	 * 表示在同一地址码所标识的区域范围内，对同年、同月、同日出生的人编定的顺序号， 顺序码的奇数分配给男性，偶数分配给女性。 5、校验码（第十八位数）
 	 * （1）十七位数字本体码加权求和公式 S = Sum(Ai * Wi), i = 0, ... , 16 ，先对前17位数字的权求和
-	 * Ai:表示第i位置上的身份证号码数字值
-	 * Wi:表示第i位置上的加权因子 
-	 * Wi: 7 9 10 5 8 4 2 1 6 3 7 9 10 5 8 4 2 
-	 * （2）计算模 Y = mod(S, 11) 
-	 * （3）通过模得到对应的校验码 Y: 0 1 2 3 4 5 6 7 8 9 10 校验码: 1 0 X 9 8 7 6 5 4 3 2
+	 * Ai:表示第i位置上的身份证号码数字值 Wi:表示第i位置上的加权因子 Wi: 7 9 10 5 8 4 2 1 6 3 7 9 10 5 8 4 2
+	 * （2）计算模 Y = mod(S, 11) （3）通过模得到对应的校验码 Y: 0 1 2 3 4 5 6 7 8 9 10 校验码: 1 0 X 9 8
+	 * 7 6 5 4 3 2
 	 */
 
 	/**
 	 * 功能：身份证的有效验证
+	 * 
 	 * @param IDStr 身份证号
 	 * @return 有效：返回"" 无效：返回String信息
-	 * @throws NumberFormatException 
+	 * @throws NumberFormatException
 	 * @throws ParseException
 	 */
-	public static String IDCardValidate(String IDStr) throws NumberFormatException, ParseException{
+	public static String IDCardValidate(String IDStr) throws NumberFormatException, ParseException {
 		String errorInfo = "";// 记录错误信息
-		String[] ValCodeArr = { "1", "0", "X", "9", "8", "7", "6", "5", "4",
-				"3", "2" };
-		String[] Wi = { "7", "9", "10", "5", "8", "4", "2", "1", "6", "3", "7",
-				"9", "10", "5", "8", "4", "2" };
+		String[] ValCodeArr = { "1", "0", "X", "9", "8", "7", "6", "5", "4", "3", "2" };
+		String[] Wi = { "7", "9", "10", "5", "8", "4", "2", "1", "6", "3", "7", "9", "10", "5", "8", "4", "2" };
 		String Ai = "";
 		// ================ 号码的长度 15位或18位 ================
 		if (IDStr.length() != 15 && IDStr.length() != 18) {
@@ -74,9 +66,7 @@ public class IDCardUtil {
 		}
 		GregorianCalendar gc = new GregorianCalendar();
 		SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd");
-		if ((gc.get(Calendar.YEAR) - Integer.parseInt(strYear)) > 150
-				|| (gc.getTime().getTime() - s.parse(
-						strYear + "-" + strMonth + "-" + strDay).getTime()) < 0) {
+		if ((gc.get(Calendar.YEAR) - Integer.parseInt(strYear)) > 150 || (gc.getTime().getTime() - s.parse(strYear + "-" + strMonth + "-" + strDay).getTime()) < 0) {
 			errorInfo = "身份证生日不在有效范围。";
 			return errorInfo;
 		}
@@ -101,9 +91,7 @@ public class IDCardUtil {
 		// ================ 判断最后一位的值 ================
 		int TotalmulAiWi = 0;
 		for (int i = 0; i < 17; i++) {
-			TotalmulAiWi = TotalmulAiWi
-					+ Integer.parseInt(String.valueOf(Ai.charAt(i)))
-					* Integer.parseInt(Wi[i]);
+			TotalmulAiWi = TotalmulAiWi + Integer.parseInt(String.valueOf(Ai.charAt(i))) * Integer.parseInt(Wi[i]);
 		}
 		int modValue = TotalmulAiWi % 11;
 		String strVerifyCode = ValCodeArr[modValue];
@@ -123,6 +111,7 @@ public class IDCardUtil {
 
 	/**
 	 * 功能：设置地区编码
+	 * 
 	 * @return Hashtable 对象
 	 */
 	private static Hashtable<String, String> GetAreaCode() {
@@ -158,15 +147,15 @@ public class IDCardUtil {
 		hashtable.put("63", "青海");
 		hashtable.put("64", "宁夏");
 		hashtable.put("65", "新疆");
-		hashtable.put("71", "台湾");
 		hashtable.put("81", "香港");
 		hashtable.put("82", "澳门");
-		hashtable.put("91", "国外");
+		hashtable.put("83", "台湾");
 		return hashtable;
 	}
 
 	/**
 	 * 功能：判断字符串是否为数字
+	 * 
 	 * @param str
 	 * @return
 	 */
@@ -182,11 +171,14 @@ public class IDCardUtil {
 
 	/**
 	 * 功能：判断字符串是否为日期格式
-	 * @param strDate date String
+	 * 
+	 * @param strDate
+	 *            date String
 	 * @return
 	 */
 	public static boolean isDate(String strDate) {
-		Pattern pattern = Pattern.compile("^((\\d{2}(([02468][048])|([13579][26]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])))))|(\\d{2}(([02468][1235679])|([13579][01345789]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))(\\s(((0?[0-9])|([1-2][0-3]))\\:([0-5]?[0-9])((\\s)|(\\:([0-5]?[0-9])))))?$");
+		Pattern pattern = Pattern.compile(
+				"^((\\d{2}(([02468][048])|([13579][26]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])))))|(\\d{2}(([02468][1235679])|([13579][01345789]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))(\\s(((0?[0-9])|([1-2][0-3]))\\:([0-5]?[0-9])((\\s)|(\\:([0-5]?[0-9])))))?$");
 		Matcher m = pattern.matcher(strDate);
 		if (m.matches()) {
 			return true;
